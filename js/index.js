bd48388d4c54aaa649b4732196bd396f9c9c230e
stringhandler <mikethetike@tari.com>
2022-10-19 18:22:28 +0200

    add dynamic ui

--- js/index.js
+++ js/index.js
@@ -11,35 +11,130 @@ function toJsonRpc(method, params){
     }
 }
 
+//
+// class TariConnectionJs {
+//
+//     // private kv: KeyRing;
+//     constructor(url) {
+//         this.url = url
+//         // init
+//
+//         // this.kv = KeyRing.new();
+//     }
+//
+//     async getIdentity() {
+//         return (await axios({
+//             method: 'post',
+//             url: this.url,
+//             data: toJsonRpc("get_identity", []),
+//
+//         })).data.result
+//     }
+// }
 
-class TariConnectionJs {
 
-    // private kv: KeyRing;
-    constructor(url) {
-        this.url = url
-        // init
+// use tari_template_macros::template;
+//
+// #[template]
+// mod counter {
+//     pub struct Counter {
+//         value: u32,
+//     }
+//
+//     impl Counter {
+//         pub fn new() -> Self {
+//             Self { value: 0 }
+//         }
+//
+//         pub fn value(&self) -> u32 {
+//             self.value
+//         }
+//
+//         pub fn increase(&mut self) {
+//             self.value += 1;
+//         }
+//     }
+// }
 
-        // this.kv = KeyRing.new();
-    }
+function setError(e) {
+    document.getElementById("alert").innerText = e;
+}
 
-    async getIdentity() {
-        return (await axios({
-            method: 'post',
-            url: this.url,
-            data: toJsonRpc("get_identity", []),
-
-        })).data.result
-    }
+function clearError() {
+    document.getElementById("alert").innerText = "";
+    document.getElementById("result").innerText = "";
 }
 
 async function main() {
     try {
         const tari = await import("../pkg/index.js").catch(console.error);
         window.tariLib = tari;
-        let conn = new tari.TariConnection("http://localhost:18145/json_rpc", "222f59f59229dda1f3453a143b12eb2947e2217664be1373a85b25d9cb3ab642");
+        let conn = new tari.TariConnection("http://localhost:18200/json_rpc", "222f59f59229dda1f3453a143b12eb2947e2217664be1373a85b25d9cb3ab642");
         let id = await conn.getIdentity();
         console.log(id);
-        let submitResponse = await conn.submitFunctionCall("D509A78BDBF2DA18733F02223547CDCB089E4E99B391E14454E57EEBD33900CB", "asf");
+        let templates = await conn.getTemplates(10);
+        console.log(templates);
+        let x = document.getElementById("templates");
+        for (let template of templates.templates){
+            let x = document.getElementById("templates");
+            var option = document.createElement("option");
+            option.text = template.address;
+            x.add(option);
+        }
+        let hTemplateName =document.getElementById("templateName");
+        let ddFunc = document.getElementById("func");
+        x.onchange = async function() {
+            try {
+                clearError();
+                window.templateAddress = x.value;
+                window.template = await conn.getTemplate(x.value);
+                console.log(template);
+                hTemplateName.innerText = template.abi.template_name;
+                for (let fn of template.abi.functions){
+                    var option = document.createElement("option");
+                    option.text = fn.name;
+                    ddFunc.add(option);
+                }
+
+            } catch (e) {
+                setError("Bad template: " + e);
+                console.error(e);
+            }
+            // let y = document.getElementById("template");
+            // y.innerHTML = template.template;
+        }
+
+        ddFunc.onchange = async function() {
+            let fn = template.abi.functions.find(x => x.name === ddFunc.value);
+            console.log(fn);
+            document.getElementById("argExample").innerText = JSON.stringify(fn.arguments, null, 2);
+        }
+
+        document.getElementById("callFunction").onclick = async function() {
+            try {
+                clearError();
+                let submitResponse = await conn.submitFunctionCall(templateAddress, ddFunc.value, JSON.parse(document.getElementById("args").value));
+                console.log(submitResponse);
+                document.getElementById("result").innerText = JSON.stringify(submitResponse, null, 2);
+            } catch (e) {
+                setError("Bad invoke: " + e);
+                console.error(e);
+            }
+        }
+
+        let btnCallMethod = document.getElementById("callMethod");
+        let txtComponentAddress = document.getElementById("componentAddress");
+        btnCallMethod.onclick = async function() {
+            try {
+                clearError();
+                let submitResponse = await conn.submitMethodCall(templateAddress, txtComponentAddress.value, ddFunc.value, JSON.parse(document.getElementById("args").value));
+                console.log(submitResponse);
+                document.getElementById("result").innerText = JSON.stringify(submitResponse, null, 2);
+            } catch (e) {
+                setError("Bad invoke: " + e);
+                console.error(e);
+            }
+        }
         // console.log(submitResponse);
         // tari.sayHello();
         // window.tari = conn;
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
-
+use serde::Deserialize;
+use serde::Serialize;
 use std::collections::HashMap;
 use std::convert::TryInto;
+use tari_crypto::keys::PublicKey;
+use tari_crypto::ristretto::{RistrettoPublicKey, RistrettoSchnorr, RistrettoSecretKey};
+use tari_crypto::tari_utilities::hex::Hex;
+use tari_crypto::tari_utilities::ByteArray;
+use tari_engine_types::hashing::hasher;
+use tari_engine_types::instruction;
+use tari_engine_types::signature::InstructionSignature;
+use tari_template_lib::models::TemplateAddress;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::console;
 use web_sys::{Request, RequestInit, RequestMode, Response};
-use wasm_bindgen::JsCast;
-use serde::Serialize;
-use serde::Deserialize;
-use tari_engine_types::hashing::hasher;
-use tari_engine_types::instruction;
-use tari_crypto::keys::PublicKey;
-use tari_crypto::ristretto::{RistrettoPublicKey, RistrettoSchnorr, RistrettoSecretKey};
-use tari_crypto::tari_utilities::ByteArray;
-use tari_crypto::tari_utilities::hex::Hex;
-use tari_engine_types::signature::InstructionSignature;
-use tari_template_lib::models::TemplateAddress;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -25,7 +24,6 @@ use tari_template_lib::models::TemplateAddress;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -34,14 +32,13 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
     Ok(())
 }
 
-#[wasm_bindgen(js_name="sayHello")]
+#[wasm_bindgen(js_name = "sayHello")]
 pub fn say_hello() -> Result<(), JsValue> {
     console::log_1(&JsValue::from_str("Hello 2"));
     Ok(())
@@ -57,27 +54,33 @@ struct JsonRequest<T> {
 
 #[derive(Serialize, Deserialize)]
 struct JsonRpcResponse<T> {
-   id: u32,
+    id: u32,
     jsonrpc: String,
     result: T,
 }
 
-async fn make_json_request<T: Serialize>(url: String, method: String, params: T) -> Result<JsValue, JsValue> {
-
+async fn make_json_request<T: Serialize>(
+    url: String,
+    method: String,
+    params: T,
+) -> Result<JsValue, JsValue> {
     let mut opts = RequestInit::new();
     opts.method("POST");
     opts.mode(RequestMode::Cors);
 
-    let s = JsonRequest{ jsonrpc: "2.0".to_string(), method: method, params: params, id: 1 };
+    let s = JsonRequest {
+        jsonrpc: "2.0".to_string(),
+        method: method,
+        params: params,
+        id: 1,
+    };
     let v = JsValue::from_str(&serde_json::to_string(&s).unwrap());
     console::log_1(&v);
     opts.body(Some(&v));
 
     let request = Request::new_with_str_and_init(&url, &opts)?;
 
-    request
-        .headers()
-        .set("Content-Type", "application/json")?;
+    request.headers().set("Content-Type", "application/json")?;
 
     let window = web_sys::window().unwrap();
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
@@ -93,7 +96,11 @@ async fn make_json_request<T: Serialize>(url: String, method: String, params: T)
     Ok(json)
 }
 
-fn sign(secret_key: &RistrettoSecretKey, secret_nonce: RistrettoSecretKey, instructions: &[instruction::Instruction]) -> InstructionSignature {
+fn sign(
+    secret_key: &RistrettoSecretKey,
+    secret_nonce: RistrettoSecretKey,
+    instructions: &[instruction::Instruction],
+) -> InstructionSignature {
     InstructionSignature::sign(secret_key, secret_nonce, instructions)
     // let public_key = RistrettoPublicKey::from_secret_key(secret_key);
     //
@@ -111,59 +118,147 @@ fn sign(secret_key: &RistrettoSecretKey, secret_nonce: RistrettoSecretKey, instr
 pub struct TariConnection {
     url: String,
     // Lol, best remove this at some point
-    secret_key: RistrettoSecretKey
+    secret_key: RistrettoSecretKey,
 }
 
 #[wasm_bindgen]
 impl TariConnection {
     #[wasm_bindgen(constructor)]
     pub fn new(url: String, secret_key_hex: String) -> TariConnection {
-        TariConnection { url, secret_key: RistrettoSecretKey::from_hex(&secret_key_hex).unwrap() }
+        TariConnection {
+            url,
+            secret_key: RistrettoSecretKey::from_hex(&secret_key_hex).unwrap(),
+        }
     }
 
-    #[wasm_bindgen(js_name="getIdentity")]
+    #[wasm_bindgen(js_name = "getIdentity")]
     pub async fn get_identity(&self) -> Result<JsValue, JsValue> {
-            let v = make_json_request(self.url.clone(), "get_identity".to_string(), HashMap::<String, String>::new()).await?;
+        let v = make_json_request(
+            self.url.clone(),
+            "get_identity".to_string(),
+            HashMap::<String, String>::new(),
+        )
+        .await?;
         let res: JsonRpcResponse<GetIdentityResponse> = serde_wasm_bindgen::from_value(v)?;
         // console::log_1(&JsValue::from_str(res.result.public_address.as_str() ));
         Ok(serde_wasm_bindgen::to_value(&res.result)?)
     }
 
+    #[wasm_bindgen(js_name = "getTemplates")]
+    pub async fn get_templates(&self, limit: Option<u32>) -> Result<JsValue, JsValue> {
+        let v = make_json_request(
+            self.url.clone(),
+            "get_templates".to_string(),
+            GetTemplatesRequest { limit: limit.unwrap_or(20) },
+        )
+            .await?;
+        console::log_1(&v);
+        let res: JsonRpcResponse<GetTemplatesResponse> = serde_wasm_bindgen::from_value(v)?;
+        // console::log_1(&JsValue::from_str(res.result.public_address.as_str() ));
+        Ok(serde_wasm_bindgen::to_value(&res.result)?)
+    }
 
+    #[wasm_bindgen(js_name = "getTemplate")]
+    pub async fn get_template(&self, address: String) -> Result<JsValue, JsValue> {
+        let v = make_json_request(
+            self.url.clone(),
+            "get_template".to_string(),
+            GetTemplateRequest {template_address: address },
+        )
+            .await?;
+        console::log_1(&v);
+        let res: JsonRpcResponse<GetTemplateResponse> = serde_wasm_bindgen::from_value(v)?;
+        // console::log_1(&JsValue::from_str(res.result.public_address.as_str() ));
+        Ok(serde_wasm_bindgen::to_value(&res.result)?)
+    }
 
-    #[wasm_bindgen(js_name="submitFunctionCall")]
-    pub async fn submit_function_call(&self, template_address: String, method: String) -> Result<JsValue, JsValue> {
+    #[wasm_bindgen(js_name = "submitFunctionCall")]
+    pub async fn submit_function_call(
+        &self,
+        template_address: String,
+        method: String,
+    ) -> Result<JsValue, JsValue> {
         // let (secret_key, public_key) = RistrettoPublicKey::random_keypair(&mut OsRng) ;
         // let instructions = vec![Instruction{
         //     template_address,
         //     method,
         //     args: vec![]
         // }];
-        let instruction = instruction::Instruction::CallFunction{ template_address: TemplateAddress::from_hex(&template_address).unwrap(),
-            function: method.clone(), args: vec![] };
-        let instructions= vec![instruction];
+        let instruction = instruction::Instruction::CallFunction {
+            template_address: TemplateAddress::from_hex(&template_address).unwrap(),
+            function: method.clone(),
+            args: vec![],
+        };
+        let instructions = vec![instruction];
         // TODO: lol better pls
-        let sec_nonce = RistrettoSecretKey::from_bytes(&[1u8;32]).unwrap();
+        let sec_nonce = RistrettoSecretKey::from_bytes(&[1u8; 32]).unwrap();
         let pub_nonce = RistrettoPublicKey::from_secret_key(&sec_nonce);
         let signature = sign(&self.secret_key, sec_nonce, &instructions);
+
         // let challenge = sign(secret_key, public_key, instructions);
-        let req = SubmitTransactionRequest{
+        let req = SubmitTransactionRequest {
             instructions: vec![Instruction{
+                type_: "CallFunction".to_string(),
                 template_address,
                 function: method,
                 args: vec![]
             }],
-            signature: Signature { signature: signature.signature().get_signature().to_hex(), public_nonce: pub_nonce.to_hex() },
+            signature: Signature {
+                signature: signature.signature().get_signature().to_hex(),
+                public_nonce: pub_nonce.to_hex(),
+            },
             fee: 0,
             sender_public_key: RistrettoPublicKey::from_secret_key(&self.secret_key).to_hex(),
-            num_new_components: 6
+            num_new_components: 2,
         };
         let v = make_json_request(self.url.clone(), "submit_transaction".to_string(), req).await?;
 
-        console::log_1(&v );
+        console::log_1(&v);
         let res: JsonRpcResponse<SubmitTransactionResponse> = serde_wasm_bindgen::from_value(v)?;
         Ok(serde_wasm_bindgen::to_value(&res.result)?)
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetTemplateRequest {
+    template_address: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetTemplateResponse {
+    registration_metadata: TemplateMetadata,
+    abi: TemplateAbi
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TemplateAbi {
+    template_name: String,
+    functions: Vec<FunctionDef>
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FunctionDef {
+    pub name: String,
+    pub arguments: Vec<String>,
+    pub output: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetTemplatesRequest {
+    limit: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GetTemplatesResponse {
+    templates: Vec<TemplateMetadata>,
+
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct TemplateMetadata {
+    address: String,
+    url: String,
+    binary_sha: Vec<u8>,
+    height: u32
 }
 
 
@@ -171,12 +266,12 @@ impl TariConnection {
 struct GetIdentityResponse {
     node_id: String,
     public_key: String,
-    public_address: String
+    public_address: String,
 }
 
 #[derive(Serialize, Deserialize)]
-struct SubmitTransactionRequest{
-   instructions: Vec<Instruction>,
+struct SubmitTransactionRequest {
+    instructions: Vec<Instruction>,
     signature: Signature,
     fee: u64,
     sender_public_key: String,
@@ -184,13 +279,15 @@ struct SubmitTransactionRequest{
 }
 
 #[derive(Serialize, Deserialize)]
-struct Signature{
+struct Signature {
     signature: String,
     public_nonce: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Instruction {
+    #[serde(rename = "type")]
+    type_: String,
     template_address: String,
     function: String,
     args: Vec<String>,
@@ -198,5 +295,5 @@ struct Instruction {
 
 #[derive(Serialize, Deserialize)]
 struct SubmitTransactionResponse {
-    hash: String
+    hash: String,
 }
